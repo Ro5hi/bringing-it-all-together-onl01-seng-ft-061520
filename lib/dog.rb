7840572800699a3166834c570c971ec14c24b2bd
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-20 15:21:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,7 +58,16 @@ class Dog
     
   end
   
-  def self.find_by_id
+  def self.find_by_id(id)
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+    SQL
+    DB[:conn].execute(sql)
+  end 
   
 end 
   
