af07ad4149fb1ebff23e5fd38197c1d1e9217cfb
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-20 15:30:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -87,8 +87,8 @@ class Dog
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else 
       dog = self.create(name: name, breed: breed)
-      
     end 
-    dog 
+  end 
+  
 
   end 
